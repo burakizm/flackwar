3b31f805d6c546b52a09a50284ac310760894678
burakizm <88186150+burakizm@users.noreply.github.com>
2021-09-02 03:25:07 +0300

    Add files via upload

---
+++ handlers/4_5872848202201304545.py
@@ -0,0 +1,136 @@
+from asyncio.queues import QueueEmpty
+
+from pyrogram import Client
+from pyrogram.types import Message
+
+import callsmusic
+
+from config import BOT_NAME as BN
+from helpers.filters import command, other_filters
+from helpers.decorators import errors, authorized_users_only
+
+from os import path
+
+from pyrogram import Client
+from pyrogram.types import Message, Voice
+
+import callsmusic
+
+import converter
+from downloaders import youtube
+
+from config import BOT_NAME as bn, DURATION_LIMIT
+from helpers.filters import command, other_filters
+from helpers.decorators import errors
+from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
+
+
+import os
+import requests
+import aiohttp
+import youtube_dl
+
+from pyrogram import filters, Client
+from youtube_search import YoutubeSearch
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+
+@Client.on_message(command("oynat") & other_filters)
+@errors
+@authorized_users_only
+async def oynat(_, message: Message):
+    audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
+    url = get_url(message)
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"**{bn} :-** 😕 Ses Dosyası Uzun {DURATION_LIMIT} minute(s) izin verilmez!\n🤐 Sağlanan ses, {audio.duration / 60} minute(s)"
+            )
+
+        file_name = get_file_name(audio)
+        file_path = await converter.convert(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name)) else file_name
+        )
+    elif url:
+        file_path = await converter.convert(youtube.download(url))
+    else:
+        return await message.reply_text(f"**{bn} :-** 🙄 Bana oynatacak bir şey vermedin.!")
+
+    if message.chat.id in callsmusic.pytgcalls.active_calls:
+        await message.reply_text(f"**{bn} :-** 😉 Sıraya Alındı. Sırası= #{await callsmusic.queues.put(message.chat.id, file_path=file_path)} !")
+    else:
+        callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
+        await message.reply_text(f"**{bn} :-** 🥳 Oynatılıyor...")
+
+
+@Client.on_message(command("durdur") & other_filters)
+@errors
+@authorized_users_only
+async def durdur(_, message: Message):
+    if (
+            message.chat.id not in callsmusic.pytgcalls.active_calls
+    ) or (
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Durduruldu'
+    ):
+        await message.reply_text(f"**{BN} :-** 🙄 Müzik açık değil!")
+    else:
+        callsmusic.pytgcalls.pause_stream(message.chat.id)
+        await message.reply_text(f"**{BN} :-** 🤐 Durduruldu!")
+
+
+@Client.on_message(command("devam") & other_filters)
+@errors
+@authorized_users_only
+async def devam(_, message: Message):
+    if (
+            message.chat.id not in callsmusic.pytgcalls.active_calls
+    ) or (
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Devam Ediyor'
+    ):
+        await message.reply_text(f"**{BN} :-** 🙄 Hiçbirşey Durdurulmadı!")
+    else:
+        callsmusic.pytgcalls.resume_stream(message.chat.id)
+        await message.reply_text(f"**{BN} :-** 🥳 Devam Ediyor!")
+
+
+@Client.on_message(command("son") & other_filters)
+@errors
+@authorized_users_only
+async def bitir(_, message: Message):
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text(f"**{BN} :-** 🙄 Hiçbirşey Oynatılmıyor!")
+    else:
+        try:
+            callsmusic.queues.clear(message.chat.id)
+        except QueueEmpty:
+            pass
+
+        callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        await message.reply_text(f"**{BN} :-** ❌ Bitti!")
+
+
+@Client.on_message(command("atla") & other_filters)
+@errors
+@authorized_users_only
+async def atla(_, message: Message):
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text(f"**{BN} :-** 🙄 Atlamak için hiçbir şey oynamıyor!")
+    else:
+        callsmusic.queues.task_done(message.chat.id)
+
+        if callsmusic.queues.is_empty(message.chat.id):
+            callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        else:
+            callsmusic.pytgcalls.change_stream(
+                message.chat.id,
+                callsmusic.queues.get(message.chat.id)["file_path"]
+            )
+
+        await message.reply_text(f"**{BN} :-** 😬 Bir Sonraki Müziğe Atlandı!")
